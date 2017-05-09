b46f48a42361b68c7daa6a6826c908d26f2fbf53
Jacob Sturm <aj.sturm10@gmail.com>
2017-05-09 19:03:41 -0400

    Refactor Message Parsing
    Moved message parsing from the chat class to the messageProvider class.
    No longer loops on searches.
    Minor tweaks to channel loading and blank message entry to prevent null
    reference exceptions

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -24,74 +24,25 @@ export class ChatPage {
   messageValue: string = '';
 
   constructor(public navCtrl: NavController, public navParams: NavParams, af: AngularFire, messageData: MessageData,
-  public channelProvider: ChannelProvider, public spotifyProvider: SpotifyProvider) {
-    this.channel = navParams.data.channel || {$key: 1};
+    public channelProvider: ChannelProvider, public spotifyProvider: SpotifyProvider) {
+    this.channel = navParams.data.channel || { $key: 1 };
     this.messageData = messageData;
     this.items = af.database.list('/channels/' + this.channel.$key);
-    this.items.subscribe(x => {this.content.scrollToBottom(0)})
+    this.items.subscribe(x => { this.content && this.content.scrollToBottom(0) })
   }
 
-// use ngmodel... look that shit up
+  // use ngmodel... look that shit up
   addMessage(message: string) {
-    this.messageData.postMessage(this.channel.$key, message).then((message) => {
-      this.messageValue = null;
-    });
+    if (message) {
+      this.messageData.postMessage(this.channel.$key, message).then((message) => {
+        this.messageValue = null;
+      });
 
-    this.parseMessage(message);
+      this.messageData.parseMessage(this.channel.$key, message);
+    }
   }
 
   unsubscribe() {
     this.channelProvider.unsubscribeUserFromChannel(this.channel.$key);
-  }
-
-  parseMessage(message: string){
-    if (message.startsWith("/search")){
-      var searchQuery = message.replace("/search","").trim();
-
-      this.spotifyProvider.search(searchQuery)
-        .subscribe(data => {
-          var response = data.tracks.items[0];
-          console.log(response);
-
-          if(response){
-            console.log(response.uri);
-            this.addMessage(response.uri);
-          }
-        });
-    } else if (message.startsWith('spotify:track:')){
-      var trackId = message.replace('spotify:track:','').trim();
-
-      this.spotifyProvider.getTrack(trackId)
-        .subscribe(data => {
-          console.log(data);
-          var artist = data.artists[0].name;
-          var album = data.album.name;
-          var trackName = data.name;
-
-          this.addMessage(artist + ' - ' + album + ' - ' + trackName);
-        })
-    } else if (message.startsWith('spotify:album:')){
-      var albumId = message.replace('spotify:album:','').trim();
-
-      this.spotifyProvider.getAlbum(albumId)
-        .subscribe(data => {
-          console.log(data);
-          var artist = data.artists[0].name;
-          var album = data.name;
-
-          this.addMessage(artist + ' - ' + album);
-        })
-    } else if (message.startsWith('spotify:artist:')){
-      var albumId = message.replace('spotify:artist:','').trim();
-
-      this.spotifyProvider.getArtist(albumId)
-        .subscribe(data => {
-          console.log(data);
-          var artist = data.name;
-          var genre = data.genres[0];
-
-          this.addMessage(artist + ' - ' + genre);
-        })
-    }
   }
 }
--- src/providers/messageProvider.ts
+++ src/providers/messageProvider.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import firebase from 'firebase';
+import { SpotifyProvider } from './spotifyProvider';
 
 
 /*
@@ -12,7 +13,7 @@ import firebase from 'firebase';
 export class MessageData {
   public messages;
 
-  constructor() {
+  constructor(public spotifyProvider: SpotifyProvider) {
     console.log('Hello MessageData Provider');
   }
 
@@ -21,5 +22,56 @@ export class MessageData {
       message: message,
       detail: 'something'
     });
+  }
+
+  parseMessage(channel, message: string) {
+    if (message.startsWith("/search")) {
+      var searchQuery = message.replace("/search", "").trim();
+
+      this.spotifyProvider.search(searchQuery)
+        .subscribe(data => {
+          var response = data.tracks.items[0];
+          console.log(response);
+
+          if (response) {
+            console.log(response.uri);
+            this.postMessage(channel, response.uri);
+          }
+        });
+    } else if (message.startsWith('spotify:track:')) {
+      var trackId = message.replace('spotify:track:', '').trim();
+
+      this.spotifyProvider.getTrack(trackId)
+        .subscribe(data => {
+          console.log(data);
+          var artist = data.artists[0].name;
+          var album = data.album.name;
+          var trackName = data.name;
+
+          this.postMessage(channel, artist + ' - ' + album + ' - ' + trackName);
+        })
+    } else if (message.startsWith('spotify:album:')) {
+      var albumId = message.replace('spotify:album:', '').trim();
+
+      this.spotifyProvider.getAlbum(albumId)
+        .subscribe(data => {
+          console.log(data);
+          var artist = data.artists[0].name;
+          var album = data.name;
+
+          this.postMessage(channel, artist + ' - ' + album);
+        })
+    } else if (message.startsWith('spotify:artist:')) {
+      var albumId = message.replace('spotify:artist:', '').trim();
+
+      this.spotifyProvider.getArtist(albumId)
+        .subscribe(data => {
+          console.log(data);
+          var artist = data.name;
+          var genre = data.genres[0];
+
+          this.postMessage(channel, artist + ' - ' + genre);
+        })
+    }
   }
 }
