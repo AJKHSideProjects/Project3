1005b179a0109c229ee5cf29ffcb1b0ca3cf7feb
Keyan Hardman <keyanhardman@gmail.com>
2017-05-23 19:30:45 -0400

    Really dirty way to post spotify widget

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -5,7 +5,7 @@ import { MessageProvider } from '../../providers/messageProvider';
 import { AuthProvider } from '../../providers/authProvider';
 import { ChannelProvider } from '../../providers/channelProvider';
 import { SpotifyProvider } from '../../providers/spotifyProvider';
-
+import { DomSanitizer } from '@angular/platform-browser';
 /*
   Generated class for the Chat page.
 
@@ -18,25 +18,37 @@ import { SpotifyProvider } from '../../providers/spotifyProvider';
 })
 
 export class ChatPage {
-  @ViewChild(Content) content: Content
+  @ViewChild(Content) content: Content;
   items: FirebaseListObservable<any[]>;
   channel;
   messageValue: string = '';
 
   constructor(public navCtrl: NavController, public navParams: NavParams, af: AngularFire, messageData: MessageProvider,
   public channelProvider: ChannelProvider, public spotifyProvider: SpotifyProvider, public authProvider: AuthProvider,
-  public messageProvider: MessageProvider) {
+  public messageProvider: MessageProvider, public sanitizer: DomSanitizer) {
     this.channel = navParams.data.channel || {$key: 1};
     this.items = af.database.list('/channels/' + this.channel.$key);
     this.items.subscribe(x => {this.content && this.content.scrollToBottom(0)})
   }
 
+  encodeURI(uri) {
+    let url = `https://embed.spotify.com/?uri=${encodeURIComponent(uri)}`
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
   addMessage(message: string) {
     let detail = {
+      spotifyUri: null,
+      created: new Date().getTime(),
       user: {
         email: this.authProvider.getCurrentUser().email
       }
     };
+
+   if (message.startsWith('spotify:track:')) {
+     detail.spotifyUri = message;
+   }
+
     if (message) {
       this.messageProvider.postMessage(this.channel.$key, message, detail).then((message) => {
         this.messageValue = null;
